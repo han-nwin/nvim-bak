0bec97e15afe9b4cbb0ab4dfd9fe851f2f9f8b5f
han-nwin <hannguyen.win@gmail.com>
2025-02-09 19:08:30 -0600

    mini nvim

---
+++ lua/plugins/mini-nvim.lua
@@ -0,0 +1,11 @@
+return {
+    {
+    "echasnovski/mini.nvim,"
+    version = "*",
+    config = function()
+        require("mini.ai").setup() -- Enable mini ai for surround text
+        require("mini.surround").setup() -- Enable surround text
+    end
+    }
+}
+
